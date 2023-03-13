b5390d2cda9ad57e58413492044606342bd678e2
noamhasingit <35137886+noamhasingit@users.noreply.github.com>
2023-03-13 22:46:26 +0200

    add fixes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import argparse
 import requests
 import json
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument("token", help= "GitHub token of the account")
+parser.add_argument("token", help="GitHub token of the account")
 args = parser.parse_args()
 TOKEN = args.token
 ALL_REQUESTS_LIST = []
@@ -14,9 +15,9 @@ def register(*args, **kwargs):
     class Wrapper:
 
         def __init__(self, func):
-           ALL_REQUESTS_LIST.append({'func': func,
-                                   'name': kwargs['name'],
-                                   'description': kwargs['description']})
+            ALL_REQUESTS_LIST.append({'func': func,
+                                      'name': kwargs['name'],
+                                      'description': kwargs['description']})
 
     return Wrapper
 
@@ -24,62 +25,94 @@ def register(*args, **kwargs):
 class ApiTool:
     @staticmethod
     def api_get(url: str, token: str = TOKEN):
-        headers = {'Accept': 'application/vnd.github+json'
-                'Authorization' f'{token}',
-                'X-GitHub-Api-Version': '2022-11-28'}
+        headers = {'Accept': 'application/vnd.github+json',
+                   'Authorization': token,
+                   'X-GitHub-Api-Version': '2022-11-28',
+                   }
+        # TODO to add pagination
         response = requests.get(url, headers=headers,  timeout=30)
         response.raise_for_status()
         return json.loads(response.content)
+
+        # while True:
+        #     response = requests.get(url, headers=headers, timeout=30)
+        #     import pdb
+        #     pdb.set_trace()
+        #     response.raise_for_status()
+        #     response_all.extend([json.loads(response.content)])
+        #     if 'Link' in response.headers and 'next' in response.headers['Link']:
+        #         link_list = response.headers['Link'].replace(' ', '').split(',')
+        #         for _link in link_list:
+        #             print(_link.replace('<', '').replace('>', ''))
+        #         print(response.headers['Link'])
+
+        #     else:
+        #         break
+        # return response_all
 
 
 @register(name="get_latest_releases",
           description="What are the latest 3 releases of CTFd?")
 def get_latest_releases(num: int = 3):
         """ 3 latests releases """
-        return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/releases")
+        releases = ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/releases")
+        published_at_list = [{'date': datetime.fromisoformat(release['published_at']),
+                              'id': release['id']} for release in releases]
+
+        published_at_list.sort(key=lambda x : x['date'], reverse=True)
+        return [item['id'] for item in published_at_list[:num]]
 
 
 @register(name="get_forks_num", description="How many forks CTFd repo has?")
 def get_forks_num():
     """ How many forks """
-    return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/forks")
+    return len(ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/forks"))
 
 
 @register(name="get_repo_stars_num", description="How many stars CTFd repo has")
 def get_repo_stars_num():
-    """ How many forks """
-    return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/stargazers")
+    """ How many stars """
+    all_repo = ApiTool.api_get("https://api.github.com/search/repositories?q=org:CTFd&sort=stars&order=desc")
+    single_repo = [item for item in all_repo['items'] if 'CTFd' in item['name']]
+    return single_repo[0]['stargazers_count']
 
 
 @register(name="get_repo_contributors_num", description="How many contributors CTFd repo has?")
 def get_repo_contributors_num():
-    """ How many forks """
-    return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/contributors")
+    """ How many contributors """
+    return len(ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/contributors"))
 
 
 @register(name="get_repo_pulls_num", description="How many pull requests CTFd repo has?")
 def get_repo_pulls_num():
     """ How many forks """
-    return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/pulls")
+    return len(ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/pulls"))
 
 
 @register(name="get_repo_commits_num", description="How many commits CTFd repo has?")
 def get_repo_commits_num():
+    """ How many commits """
+    return len(ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/commits?per_page=100"))
+
+
+@register(name="get_repo_commits_contributors_num", description="descending order list of contributors per amount of commits")
+def get_repo_commits_contributors_num():
+    """ descending order list of contributors per amount of commits """
+    print(ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/commits"))
+    return len(ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/commits"))
+
+
+@register(name="get_repo_pulls_contributors_num", description="descending order list of contributors per amount of pull requests")
+def get_repo_pulls_contributors_num():
     """ How many forks """
-    return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/commits")
+    return len(ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/pulls"))
 
 
 def main():
     for item in ALL_REQUESTS_LIST:
-        num = item['func']()
-        print(f"{item['description']} - {len(num)}")
+        result = item['func']()
+        print(f"{item['description']} - {result}")
 
 
 if __name__ == "__main__":
     main()
-
-
-
-# Create a descending order list of contributors per amount of commits
-
-# Create a descending order list of contributors per amount of pull requests
