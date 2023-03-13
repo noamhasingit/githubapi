750b743c65888e0d5cc2c4b357d18fdc5657fb42
Noam Hasin <noamhasin@gmail.com>
2023-03-13 15:27:21 +0200

    phase 1 addinf platform

---
+++ main.py
@@ -0,0 +1,69 @@
+import argparse
+import requests
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("token", help= "GitHub token of the account")
+args = parser.parse_args()
+TOKEN = args.token
+ALL_REQUESTS_LIST = []
+
+def register(*args, **kwargs):
+
+    class Wrapper:
+        def __init__(self, func):
+           ALL_REQUESTS_LIST.append({'func': func,
+                                   'name': kwargs['name'],
+                                   'description': kwargs['description']})
+
+    return Wrapper
+
+class ApiTool:
+    @staticmethod
+    def api_get(url: str, token: str = TOKEN):
+        headers = {'Accept': 'application/vnd.github+json'
+                'Authorization' f'{token}',
+                'X-GitHub-Api-Version': '2022-11-28'}
+        response = requests.get(url, headers=headers,  timeout=30)
+        response.raise_for_status()
+        return json.loads(response.content)
+
+
+@register(name = "get_latest_releases",
+          description = "What are the latest 3 releases of CTFd?")
+def get_latest_releases(num: int = 3):
+        """ 3 latests releases """
+        return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/releases")
+
+@register(name = "get_forks_num", description = "How many forks CTFd repo has?")
+def get_forks_num():
+    """ How many forks """
+    return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/forks")
+
+@register(name = "get_repo_stars_num", description =
+          "How many stars CTFd repo has")
+def get_repo_stars_num():
+    """ How many forks """
+    return ApiTool.api_get("https://api.github.com/repos/CTFd/CTFd/stars")
+
+
+def main():
+    import pdb
+    pdb.set_trace()
+    for item in ALL_REQUESTS_LIST:
+        num = item['func']()
+        print(f"{item['description']} - {len(num)}")
+
+if __name__ == "__main__":
+    main()
+
+
+# How many contributors CTFd repo has?
+
+# How many pull requests CTFd repo has?
+
+# How many commits CTFd repo has?
+
+# Create a descending order list of contributors per amount of commits
+
+# Create a descending order list of contributors per amount of pull requests
